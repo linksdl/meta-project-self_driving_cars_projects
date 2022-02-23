3a6ae130f1ea48eebb29def3cddf25e0a711f63b
Daolin Sheng <15900859362@163.com>
2022-02-23 17:19:28 +0800

    exercise 1 for calculate the iou

--- self-driving-car-engineer/exercise01-choosing_metrics/iou.py
+++ self-driving-car-engineer/exercise01-choosing_metrics/iou.py
@@ -57,3 +57,16 @@ def calculate_ious(gt_bboxes, pred_bboxes):
         for j, pred_bbox in enumerate(pred_bboxes):
             ious[i, j] = calculate_iou(gt_bbox, pred_bbox)
     return ious
+
+
+if __name__ == "__main__":
+    ground_truth, predictions = get_data()
+    # get bboxes array
+    filename = 'segment-1231623110026745648_480_000_500_000_with_camera_labels_38.png'
+    gt_bboxes = [g['boxes'] for g in ground_truth if g['filename'] == filename][0]
+    gt_bboxes = np.array(gt_bboxes)
+    pred_bboxes = [p['boxes'] for p in predictions if p['filename'] == filename][0]
+    pred_boxes = np.array(pred_bboxes)
+
+    ious = calculate_ious(gt_bboxes, pred_boxes)
+    check_results(ious)
