23154011f0129117170296598993818575ee7905
Daolin Sheng <15900859362@163.com>
2022-02-24 11:30:03 +0800

    update the data

---
+++ self-driving-car-engineer/exercise03-tfrecord/utils.py
@@ -0,0 +1,14 @@
+"""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+@Time        : 2022/2/23 20:12
+@Author      : shengdl999links@gmail.com
+@ProjectName : self-driving-car-engineer
+@File        : utils.py
+"""
+
+import tensorflow.compat.v1 as tf
+import w
+
+
+
