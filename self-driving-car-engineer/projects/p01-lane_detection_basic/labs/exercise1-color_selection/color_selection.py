d37c57b9ad5419cbc02d55d43923237a45062788
Daolin Sheng <15900859362@163.com>
2022-02-28 13:51:05 +0800

    exercise for color selection

--- self-driving-car-engineer/projects/p01-lane_detection_basic/labs/exercise1-color_selection/color_selection.py
+++ self-driving-car-engineer/projects/p01-lane_detection_basic/labs/exercise1-color_selection/color_selection.py
@@ -26,6 +26,11 @@ color_select = np.copy(image)
 # Define our color selection criteria
 # Note: if you run this code, you'll find these are not sensible values!!
 # But you'll get a chance to play with them soon in a quiz
+"""
+Next I define a color threshold in the variables red_threshold, green_threshold, 
+and blue_threshold and populate rgb_threshold with these values.
+This vector contains the minimum values for red, green, and blue (R,G,B) that I will allow in my selection.
+"""
 red_threshold = 200
 green_threshold = 200
 blue_threshold = 200
@@ -35,11 +40,12 @@ rgb_threshold = [red_threshold, green_threshold, blue_threshold]
 thresholds = (image[:,:,0] < rgb_threshold[0]) \
             | (image[:,:,1] < rgb_threshold[1]) \
             | (image[:,:,2] < rgb_threshold[2])
+# print(thresholds)
 color_select[thresholds] = [0,0,0]
 
 # Display the image
 plt.imshow(color_select)
-# plt.show()
+plt.show()
 
 # Uncomment the following code if you are running the code locally and wish to save the image
 mpimg.imsave("test-after.png", color_select)
