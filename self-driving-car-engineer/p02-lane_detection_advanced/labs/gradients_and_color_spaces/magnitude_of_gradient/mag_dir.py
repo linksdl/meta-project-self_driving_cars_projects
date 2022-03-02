38f50f03b22fcfd0378dd6ae30d835cc1abe888d
Daolin Sheng <15900859362@163.com>
2022-03-02 13:44:36 +0800

    direction of gradient

---
+++ self-driving-car-engineer/p02-lane_detection_advanced/labs/gradients_and_color_spaces/magnitude_of_gradient/mag_dir.py
@@ -0,0 +1,46 @@
+"""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+@Time        : 2022/2/28 20:51
+@Author      : shengdl999links@gmail.com
+@ProjectName : self-driving-car-engineer
+@File        : mag_dir.py
+"""
+
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pickle
+
+
+# Read in an image
+image = mpimg.imread('signs_vehicles_xygrad.png')
+
+# Define a function that applies Sobel x and y,
+# then computes the magnitude of the gradient
+# and applies a threshold
+
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    # 2) Take the gradient in x and y separately
+    # 3) Calculate the magnitude
+    # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+    # 5) Create a binary mask where mag thresholds are met
+    # 6) Return this mask as your binary_output image
+    binary_output = np.copy(img)  # Remove this line
+    return binary_output
+
+
+# Run the function
+mag_binary = mag_thresh(image, sobel_kernel=3, mag_thresh=(30, 100))
+# Plot the result
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(mag_binary, cmap='gray')
+ax2.set_title('Thresholded Magnitude', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
--- self-driving-car-engineer/p02-lane_detection_advanced/labs/gradients_and_color_spaces/magnitude_of_gradient/mag_dir_solution.py
+++ self-driving-car-engineer/p02-lane_detection_advanced/labs/gradients_and_color_spaces/magnitude_of_gradient/mag_dir_solution.py
@@ -1,11 +1,12 @@
 """
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-@Time        : 2022/2/25 14:45
+@Time        : 2022/2/28 20:51
 @Author      : shengdl999links@gmail.com
 @ProjectName : self-driving-car-engineer
-@File        : mag_thresh.py
+@File        : mag_dir.py
 """
+
 
 import numpy as np
 import cv2
@@ -13,28 +14,28 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import pickle
 
+
 # Read in an image
 image = mpimg.imread('signs_vehicles_xygrad.png')
-
 
 # Define a function that applies Sobel x and y,
 # then computes the magnitude of the gradient
 # and applies a threshold
+
 def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
     # Apply the following steps to img
     # 1) Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
     # 2) Take the gradient in x and y separately
-    soblex = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobley = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Take both Sobel x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
 
     # 3) Calculate the magnitude
-    gradmag = np.sqrt(soblex**2 + sobley**2)
+    gradmag = np.sqrt( sobelx ** 2 + sobely ** 2 )
 
     # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
-    scale_factor = np.max(gradmag) / 255
-    gradmag = (gradmag/scale_factor).astype(np.uint8)
+    gradmag = np.uint8(255 * gradmag / np.max(gradmag))
 
     # 5) Create a binary mask where mag thresholds are met
     binary_output = np.zeros_like(gradmag)
@@ -46,7 +47,7 @@ def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
 
 
 # Run the function
-mag_binary = mag_thresh(image, sobel_kernel=3, mag_thresh=(30, 100))
+mag_binary = mag_thresh(image, sobel_kernel=9, mag_thresh=(30, 100))
 # Plot the result
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
@@ -55,5 +56,4 @@ ax1.set_title('Original Image', fontsize=50)
 ax2.imshow(mag_binary, cmap='gray')
 ax2.set_title('Thresholded Magnitude', fontsize=50)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-
 plt.show()
